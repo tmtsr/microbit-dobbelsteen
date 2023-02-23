d17f2453535fc3e4f942b242ff18448d53ad27f6
tmtsr <117850984+tmtsr@users.noreply.github.com>
2023-02-23 15:55:47 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,62 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Chessboard)
+    basic.pause(1000)
+    getal = randint(1, 6)
+    if (getal == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (getal == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (getal == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (getal == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (getal == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (getal == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+let getal = 0
+basic.showIcon(IconNames.Yes)
